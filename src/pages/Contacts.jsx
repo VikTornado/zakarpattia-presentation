da1313ed0c054596e610634ff50cb850689f4297
React <3809164@gmail.com>
2025-04-01 10:33:45 +0300

    Оновлено сторінку Контакти (видалено соцмережі)

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,12 +1,4 @@
 import React from "react";
-import {
-  FaFacebookF,
-  FaInstagram,
-  FaViber,
-  FaWhatsapp,
-  FaTelegramPlane,
-  FaEnvelope,
-} from "react-icons/fa";
 import ContactForm from "../components/ContactForm";
 
 function Contacts({ language }) {
@@ -41,48 +33,9 @@ function Contacts({ language }) {
           <p>economy@carpathia.gov.ua</p>
           <p>(0312) 61-53-05, 61-29-18</p>
         </div>
+
         <div>
           <ContactForm language={language} />
-        </div>
-
-        Соцмережі
-        <div className="flex gap-4 justify-center mt-8 flex-wrap">
-          {[
-            { icon: <FaFacebookF />, link: "#", label: "Facebook" },
-            { icon: <FaInstagram />, link: "#", label: "Instagram" },
-            { icon: <FaViber />, link: "#", label: "Viber" },
-            { icon: <FaWhatsapp />, link: "#", label: "WhatsApp" },
-            { icon: <FaTelegramPlane />, link: "#", label: "Telegram" },
-            {
-              icon: <FaEnvelope />,
-              link: "mailto:info@example.com",
-              label: "Email",
-            },
-          ].map((item, index) => (
-            <div className="relative group" key={index}>
-              <a
-                href={item.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                className={`
-          w-12 h-12 flex items-center justify-center
-          text-white text-xl rounded-full
-          bg-gradient-to-t from-gray-700 to-gray-900 dark:from-gray-300 dark:to-gray-500
-          shadow-lg transition-all duration-500 ease-in-out
-          hover:scale-110 hover:ring-4 hover:ring-blue-400
-          animate-[pulse_3s_ease-in-out_infinite]
-        `}
-                style={{ animationDelay: `${index * 0.4}s` }}
-              >
-                {item.icon}
-              </a>
-
-              {/* Tooltip */}
-              <div className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 px-2 py-1 text-xs rounded bg-black text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                {item.label}
-              </div>
-            </div>
-          ))}
         </div>
       </div>
     </div>
